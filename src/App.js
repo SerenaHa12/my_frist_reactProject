9643ce53adab09fc152e0f12b0f6d2edbd6202e8
SerenaHa12 <serenavander.ha@gmail.com>
2023-06-10 20:53:22 +0700

    get API from omdbapi.com, console log object

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// d5581b28
+const API_URL = 'http://www.omdbapi.com?apikey=d5581b28';
+
+const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('SpiderMan');
+    }, []);
+
+    return (
+        <h1>Hello</h1>
+    );
 }
 
 export default App;
