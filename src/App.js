525557c1255817f8254524811e489a7b7547292c
SerenaHa12 <serenavander.ha@gmail.com>
2023-06-10 23:07:32 +0700

    search function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import MovieCard from './MovieCard';
+import SearchIcon from './search.svg';
+import './App.css';
 
-// d5581b28
 const API_URL = 'http://www.omdbapi.com?apikey=d5581b28';
 
 const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
 
-        console.log(data.Search);
-    }
+        setMovies(data.Search || []);
+    };
 
     useEffect(() => {
         searchMovies('SpiderMan');
     }, []);
 
+    const handleSearch = () => {
+        searchMovies(searchTerm);
+    };
+
     return (
-        <h1>Hello</h1>
+        <div className='app'>
+            <h1>MovieLand</h1>
+
+            <div className='search'>
+                <input
+                    placeholder='Search'
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <img 
+                    src={SearchIcon}
+                    alt='search'
+                    onClick={handleSearch}
+                />
+            </div>
+
+            {movies.length > 0 ? (
+                <div className='container'>
+                    {movies.map((movie) => (
+                        <MovieCard movie={movie} key={movie.imdbID} />
+                    ))}
+                </div>
+            ) : (
+                <div className='empty'>
+                    <h2>No movies</h2>
+                </div>
+            )}
+        </div>
     );
 }
 
